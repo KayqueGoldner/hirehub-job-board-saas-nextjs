5aaff6b6f21a4832918db421a289bac9612e118a
Kayque Goldner <kayquegoldner@gmail.com>
2025-01-27 01:11:17 -0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="relative flex size-full text-lg font-bold text-blue-500">
+        Hello World
+      </h1>
+    </div>
+  );
+}
