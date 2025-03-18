1177b5ba2474334f0f663ef4f125c837a21e8fc0
Kayque Goldner <kayquegoldner@gmail.com>
2025-03-17 22:27:54 -0300

    send periodic job listings emails

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -2,10 +2,14 @@ import { serve } from "inngest/next";
 
 import { inngest } from "@/app/utils/inngest/client";
 
-import { handleJobExpiration, helloWorld } from "./functions";
+import {
+  handleJobExpiration,
+  helloWorld,
+  sendPeriodicJobListings,
+} from "./functions";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [helloWorld, handleJobExpiration],
+  functions: [helloWorld, handleJobExpiration, sendPeriodicJobListings],
 });
--- components/general/job-filters.tsx
+++ components/general/job-filters.tsx
@@ -20,7 +20,7 @@ const jobTypes = ["full-time", "part-time", "contract", "internship"];
 
 export const JobFilters = () => {
   return (
-    <Card className="col-span-1">
+    <Card className="col-span-1 h-fit">
       <CardHeader className="flex flex-row items-center justify-between">
         <CardTitle className="text-2xl font-semibold">Filters</CardTitle>
         <Button variant="destructive" size="sm" className="h-8">
