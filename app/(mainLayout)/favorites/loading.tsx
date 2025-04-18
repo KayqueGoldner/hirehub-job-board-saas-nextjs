b08261eaa2fa8b8ec71bd9bd3efceb021be8f047
Kayque Goldner <kayquegoldner@gmail.com>
2025-04-17 21:17:57 -0300

    favorites page skeleton

---
+++ app/(mainLayout)/favorites/loading.tsx
@@ -0,0 +1,7 @@
+import { JobListingsSkeleton } from "@/components/general/job-listings-skeleton";
+
+const LoadingFavorites = () => {
+  return <JobListingsSkeleton />;
+};
+
+export default LoadingFavorites;
